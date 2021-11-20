52054d2c584b6d59346f85fd08ac46ae4b4372fa
dankastle <daniel.castillo.e@gmail.com>
2021-11-19 19:33:34 -0500

    Update GetVmInstances.sh

--- GetVmInstances.sh
+++ GetVmInstances.sh
@@ -1,9 +1,9 @@
 #The below line will do an action for every project in the project list
 for project in $(gcloud projects list --format='(project_id)' | egrep -v 'PROJECT_ID');
-    do
-		#This gcloud command will run for every instance of project in projectlist
-		gcloud config set project $project
-		printf "$project\n $(gcloud compute instances list)\n\n"
+do
+  #This gcloud command will run for every instance of project in projectlist
+  gcloud config set project $project
+  printf "$project\n $(gcloud compute instances list)\n\n"
 
-			#ouput to csv
-			done >>output.csv
+#ouput to csv
+done >>output.csv
