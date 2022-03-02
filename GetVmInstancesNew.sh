97a5b400142603dc15c1fde08d6c6ae3b1196524
Daniel Castillo E <daniel.castillo.e@gmail.com>
2022-03-01 20:42:49 -0500

    Update GetVmInstancesNew.sh

--- GetVmInstancesNew.sh
+++ GetVmInstancesNew.sh
@@ -5,7 +5,7 @@
 # 19/11/2021
 #######################################
 #The below line will do an action for every project in the project list
-for project in $(cat projects_new); do
+for project in $(cat projects); do
   #This gcloud command will run for every instance of project in projectlist
   gcloud config set project $project
   printf "$project\n $(gcloud compute instances list --filter="status:(RUNNING)")\n"
