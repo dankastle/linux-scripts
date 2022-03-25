37fed14846a305c5825fe57161af39f48269ba56
dankastle <daniel.castillo.e@gmail.com>
2022-03-25 16:38:03 +0000

    Added every gcp project as an array loop

--- getVmInstancesNew.sh
+++ getVmInstancesNew.sh
@@ -6,12 +6,18 @@
 #######################################
 #The below line will do an action for every project in the project list
 for project in $(cat projects.txt); do
-  #This gcloud command will run for every instance of project in projectlist
-  gcloud config set project $project
-  nameInst=$(gcloud compute instances list --filter="status:(RUNNING)" | head -2 | tail -1 | awk '{print $1}')
-  zoneInst=$(gcloud compute instances list --filter="status:(RUNNING)" | head -2 | tail -1 | awk '{print $2}')
-  printf "\n$(gcloud compute instances list --filter="status:(RUNNING)" --format="table[box,margin=4,title='Instances on $project'](NAME,ZONE,MACHINE_TYPE,INTERNAL_IP,STATUS)") \
-  \n$(gcloud compute instances os-inventory describe $nameInst --zone $zoneInst | grep -A 9 Architecture)\n"
+ #This gcloud command will run for every instance of project in projectlist
+ gcloud config set project $project
+ os="Ubuntu"
+ printf "\n$(gcloud compute instances os-inventory list-instances --os-shortname="$os" --filter="status:(RUNNING)" --format="table[box,margin=2,title='Instances on $project running $os'](NAME,ZONE,MACHINE_TYPE,INTERNAL_IP,STATUS)")\n"
+   mapfile -t nameInst < <( gcloud compute instances os-inventory list-instances --os-shortname="$os" --filter="status:(RUNNING)" | tail -n +2 | awk '{print $1}' )
+   mapfile -t zoneInst < <( gcloud compute instances os-inventory list-instances --os-shortname="$os" --filter="status:(RUNNING)" | tail -n +2 | awk '{print $2}' )
+   for index in ${!nameInst[*]}; do 
+     printf "\n$(gcloud compute instances os-inventory describe ${nameInst[$index]} --zone ${zoneInst[$index]} | grep -A 9 Architecture)\n "
+   done
 
-#ouput to csv
+   #ouput to csv
 done >>output.csv
+
+
+
